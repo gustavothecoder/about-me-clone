e289f06b9d05f870b786d8d08a0a9b5b9d21a416
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-06-08 22:28:47 -0300

    Creating the Color model and its tests

---
+++ spec/models/color_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Color, type: :model do
+  before(:all) do
+    @color = Color.new(color: 'Blue')
+  end
+
+  after(:all) do
+    @color = nil
+  end
+
+  it 'Must be a Color instance' do
+    expect(@color).to be_instance_of(Color)
+  end
+
+  it 'Must be successfully registered' do
+    expect(@color.save).to be true
+  end
+
+  it 'Must be registered color' do
+    expect(@color.color).to eq 'Blue'
+  end
+
+  it 'Must not be registered' do
+    second_color= Color.new
+    expect(second_color.save).to be false
+  end
+end
