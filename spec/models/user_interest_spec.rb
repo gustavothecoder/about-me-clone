730df3ffbc07dcb4e585571713d646b4fb73e052
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-06-18 22:59:47 -0300

    Adding tests on user_interest_spec

--- spec/models/user_interest_spec.rb
+++ spec/models/user_interest_spec.rb
@@ -13,13 +13,12 @@ RSpec.describe UserInterest, type: :model do
       location: 'Piracicaba - SP'
     )
     @user.save
-    @user_interest = UserInterest.new(user_id: @user.id, interest_id: @interest.id)
+    @user_interest = UserInterest.new(user: @user, interest: @interest)
   end
 
   after(:all) do
     @interest.destroy
     @user.destroy
-    @user_interest.destroy
   end
 
   it 'Must be a UserInterest instance' do
@@ -39,21 +38,58 @@ RSpec.describe UserInterest, type: :model do
       expect(@user_interest.interest).to eq @interest
     end
   end
+  
+  context 'Relationship tests' do
+    before(:all) do
+      @user_interest.save
+      @second_user = User.new(
+        username: 'gXhr',
+        email: 'gustavoh@gmail.com',
+        password: '123456',
+        first_name: 'Gustavo',
+        last_name: 'Ribeiro',
+        location: 'Piracicaba - SP'
+      )
+      @second_user.save
+      @second_user_interest = UserInterest.new(user: @second_user, interest: @interest)
+      @second_user_interest.save
+    end
 
+    after(:all) do
+      @second_user.destroy
+    end
+
+    it 'Must not be empty - interest' do
+      expect(@interest.users.empty?).to be false 
+    end
+    
+    it 'Must belong to two users' do
+      expect(@interest.users.length).to eq 2
+    end
+
+    it 'Must not be empty - user' do
+      expect(@user.interests.empty?).to be false
+    end
+
+    it 'Must have an interest' do
+      expect(@user.interests.length).to eq 1
+    end
+  end
+  
   context 'Invalid records' do
     it 'Must not be registered #1' do
-      second_user_interest = UserInterest.new
-      expect(second_user_interest.save).to be false
+      third_user_interest = UserInterest.new
+      expect(third_user_interest.save).to be false
     end
     
     it 'Must not be registered #2' do
-      second_user_interest = UserInterest.new(user_id: @user.id)
-      expect(second_user_interest.save).to be false
+      third_user_interest = UserInterest.new(user_id: @user.id)
+      expect(third_user_interest.save).to be false
     end
     
     it 'Must not be registered #3' do
-      second_user_interest = UserInterest.new(interest_id: @interest.id)
-      expect(second_user_interest.save).to be false
+      third_user_interest = UserInterest.new(interest_id: @interest.id)
+      expect(third_user_interest.save).to be false
     end
   end
 end
