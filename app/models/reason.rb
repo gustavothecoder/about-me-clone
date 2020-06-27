9cf5de233d1a831ce63161aca4e238e439f3b4a4
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-06-27 15:25:34 -0300

    Defining the relationship between user and reason

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,3 +1,5 @@
 class Reason < ApplicationRecord
   validates :reason, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   
   has_one_attached :photo, dependent: :destroy
   has_and_belongs_to_many :interests, dependent: :destroy
+  has_and_belongs_to_many :reasons, dependent: :destroy
 end
