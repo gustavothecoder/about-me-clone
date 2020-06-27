2b8836bc02682822dafc7c9ec5f873d4b6dbd488
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-06-27 15:34:14 -0300

    Refactoring and adding more tests to user_interest model

--- spec/models/user_interest_spec.rb
+++ spec/models/user_interest_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe UserInterest, type: :model do
       @second_user.destroy
     end
 
-    it 'Must not be empty - interest' do
+    it 'There must be a relationship with a user' do
       expect(@interest.users.empty?).to be false 
     end
     
@@ -67,12 +67,36 @@ RSpec.describe UserInterest, type: :model do
       expect(@interest.users.length).to eq 2
     end
 
-    it 'Must not be empty - user' do
+    it 'Must be equal to @user' do
+      expect(@interest.users[0]).to eq @user
+    end
+
+    it 'Must be equal to @second_user' do
+      expect(@interest.users[1]).to eq @second_user
+    end
+
+    it '#1 There must be a relationship with a interest' do
       expect(@user.interests.empty?).to be false
     end
 
-    it 'Must have an interest' do
+    it '#1 Must have an interest' do
       expect(@user.interests.length).to eq 1
+    end
+
+    it '#1 Must be equal to @interest' do
+      expect(@user.interests[0]).to eq @interest
+    end
+    
+    it '#2 There must be a relationship with a interest' do
+      expect(@second_user.interests.empty?).to be false
+    end
+
+    it '#2 Must have an interest' do
+      expect(@second_user.interests.length).to eq 1
+    end
+
+    it '#2 Must be equal to @interest' do
+      expect(@second_user.interests[0]).to eq @interest
     end
   end
   
