cc65601ec89b7e074d5517200a2fb910e69bb9fb
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-07-09 20:35:38 -0300

    Refactoring design_spec to use factory bot

--- spec/models/design_spec.rb
+++ spec/models/design_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Design, type: :model do
   context 'Registering a design' do
     before(:all) do
-      @design = Design.new(design_type: 'Small Design')
+      @design = create :design
     end
-  
+
     after(:all) do
       @design.destroy
     end
-  
+
     it 'Must be an instance of Design' do
-      expect(@design).to be_instance_of(Design)
+      expect(@design).to be_instance_of Design
     end
-  
+
     it 'Must be successfully registered' do
       expect(@design.save).to be true
     end
-  
+
     it 'Must be registered design' do
-      expect(@design.design_type).to eq 'Small Design'
+      expect(@design.design_type).to eq 'small'
     end
-  
+
     it 'Must not be registered' do
       second_design = Design.new
       expect(second_design.save).to be false
