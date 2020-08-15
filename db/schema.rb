4a80e9c634794ed7dc407843a1110c318bc1a497
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-08-14 22:56:11 -0300

    Renaming column password to password_digest in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_185236) do
+ActiveRecord::Schema.define(version: 2020_08_15_014254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_185236) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
     t.string "location"
