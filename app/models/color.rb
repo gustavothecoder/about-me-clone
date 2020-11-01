f713360a000db8818266669edf73054955bd0abe
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-11-01 19:59:56 -0300

    Use validates_presence_of for the color attribute

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Color < ApplicationRecord
-  validates :color, presence: true
+  validates_presence_of :color
 
   has_many :user_page_design
   has_many :users, through: :user_page_design
