1c23235f3c9b3de7f5710b9d6437250bee495fc1
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-11-01 19:45:28 -0300

    Refactor color_spec

--- spec/models/color_spec.rb
+++ spec/models/color_spec.rb
@@ -1,30 +1,47 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Color, type: :model do
-  context 'Registering a color' do
-    before :all do
-      @color = create :color
+  let(:color) { create(:color) }
+
+  context 'When the color attribute is filled' do
+    it 'Must be valid' do
+      expect(color).to be_valid
     end
-  
-    after :all do
-      @color.destroy
+  end
+
+  context 'When the color attribute is not filled' do
+    it 'Must not be valid' do
+      color.color = nil
+      color.save
+      expect(color).to_not be_valid
+    end
+  end
+
+  describe 'Relationships' do
+    let(:reason) { create(:reason) }
+    let(:user) { create(:user, reason: reason) }
+    let(:design) { create(:design) }
+    let!(:user_page_design) do
+      create(:user_page_design, user: user, design: design, color: color)
     end
 
-    it 'Must be a Color instance' do
-      expect(@color).to be_instance_of(Color)
+    describe 'has one user_page_design' do
+      it 'Must have a user_page_design' do
+        expect(color.user_page_design).to_not be_nil
+      end
     end
-  
-    it 'Must be successfully registered' do
-      expect(@color.save).to be true
-    end
-  
-    it 'Must be registered color' do
-      expect(@color.color).to eq 'blue'
-    end
-  
-    it 'Must not be registered' do
-      second_color= Color.new
-      expect(second_color.save).to be false
+
+    describe 'has many users' do
+      let(:second_user) { create(:user, reason: reason) }
+      let!(:second_user_page_design) do
+        create(:user_page_design, user: second_user, design: design, color: color)
+      end
+
+      it 'Must have two users' do
+        expect(color.users.count).to eq(2)
+      end
     end
   end
 end
