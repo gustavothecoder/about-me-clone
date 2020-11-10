9f8d96e68f14eabe3f0e63726f6ae13434ca6d28
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-11-09 21:41:01 -0300

    Refactor home_controller_spec

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe 'Home controller', type: :request do
   context 'GET home#homepage' do
-    it 'Must have http 200 status' do
-      get '/'
-      expect(response).to have_http_status(:ok)
-    end
-
     it 'Must render the homepage view' do
       get '/'
+      expect(response).to have_http_status(200)
       expect(response).to render_template(:homepage)
     end
   end
