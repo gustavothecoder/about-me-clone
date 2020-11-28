96a2ca50231879dcd4048b84fe56a87345a03c23
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-11-28 20:32:00 -0300

    Add users#next_signup_step tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template(:new)
     end
   end
+
+  describe 'POST /next_signup_step/:step' do
+    context 'When an ajax call is made' do
+      it 'Must render the next_signup_step template' do
+        post '/next_signup_step/2', xhr: true
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:next_signup_step)
+      end
+    end
+
+    context 'When a default request is made' do
+      it 'ActionController::UnknownFormat exception should be thrown' do
+        expect { post '/next_signup_step/2' }.to raise_error(ActionController::UnknownFormat)
+      end
+    end
+  end
 end
