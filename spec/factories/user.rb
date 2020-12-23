a5bab2ef1dd88ac61b61ac37d4b7630faeb180ba
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-12-23 20:59:49 -0300

    Remove reason build from user's factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -10,7 +10,6 @@ FactoryBot.define do
     first_name { 'Gustavo' }
     last_name { 'Ribeiro' }
     location { 'Piracicaba' }
-    reason { build(:reason) }
     photo { Rack::Test::UploadedFile.new('public/test_active_storage.jpg') }
   end
 end
