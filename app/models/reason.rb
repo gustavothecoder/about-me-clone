63b925cd356b2560e7421b73cc6c21c0c142bea6
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-12-24 20:34:51 -0300

    Define relationship between reason and user reason

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -3,5 +3,6 @@
 class Reason < ApplicationRecord
   validates :reason, presence: true, uniqueness: true
 
-  has_many :users
+  has_many :user_reasons
+  has_many :users, through: :user_reasons
 end
