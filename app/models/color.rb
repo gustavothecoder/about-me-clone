fa4d6df871400d9b16a843fe05528a61e998aa7e
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-02 15:51:51 -0300

    Delete model color

--- app/models/color.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Color < ApplicationRecord
-  validates_presence_of :color
-
-  has_many :user_page_designs
-  has_many :users, through: :user_page_designs
-end
