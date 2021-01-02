5bfac0faebf878838c22af0193c888c7b91d0535
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-02 19:50:56 -0300

    Remove design model

--- app/models/design.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Design < ApplicationRecord
-  validates_presence_of :design_type
-
-  has_many :user_page_designs
-  has_many :users, through: :user_page_designs
-end
