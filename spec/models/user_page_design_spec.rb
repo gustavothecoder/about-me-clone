b3f5d85f289ab863f9cd5f226f3cd2d55da1c434
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-02 20:15:54 -0300

    Adjust the user page design tests after remove the relation to the color table

--- spec/models/user_page_design_spec.rb
+++ spec/models/user_page_design_spec.rb
@@ -5,8 +5,7 @@ require 'rails_helper'
 RSpec.describe UserPageDesign, type: :model do
   let(:reason) { create(:reason) }
   let(:user) { create(:user) }
-  let(:design) { create(:design) }
-  let(:user_page_design) { create(:user_page_design, user: user, design: design, color: :blue) }
+  let(:user_page_design) { create(:user_page_design, user: user, design: :small, color: :blue) }
 
   context 'When all parameters are filled' do
     it 'Must be valid' do
@@ -16,14 +15,7 @@ RSpec.describe UserPageDesign, type: :model do
 
   context 'When user is not filled' do
     it 'Must not be valid' do
-      invalid_user_page_design = build(:user_page_design, user: nil, design: design)
-      expect(invalid_user_page_design).to_not be_valid
-    end
-  end
-
-  context 'When design is not filled' do
-    it 'Must not be valid' do
-      invalid_user_page_design = build(:user_page_design, design: nil)
+      invalid_user_page_design = build(:user_page_design, user: nil)
       expect(invalid_user_page_design).to_not be_valid
     end
   end
@@ -31,10 +23,6 @@ RSpec.describe UserPageDesign, type: :model do
   describe 'Relationships' do
     describe 'belongs_to user' do
       it { expect(user_page_design.user).to eq(user) }
-    end
-
-    describe 'belongs_to design' do
-      it { expect(user_page_design.design).to eq(design) }
     end
   end
 end
