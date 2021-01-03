ec2ebfad5d2b13ace41ce710fb5d6e7fbadccfb2
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-03 14:36:03 -0300

    Extract check_that_the_data_sent_is_already_in_use from the next_signup_step action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,16 @@ class UsersController < ApplicationController
   def next_signup_step
     @step = params[:step]
     @reason = Reason.find(params[:reason]) if @step == '7'
-    @step = 'email in use' if User.find_by_email(params[:email]).present?
-    @step = 'username in use' if User.find_by_username(params[:username]).present?
+    check_that_the_data_sent_is_already_in_use
     respond_to do |format|
       format.js
     end
+  end
+
+  private
+
+  def check_that_the_data_sent_is_already_in_use
+    @step = 'email in use' if User.find_by_email(params[:email]).present?
+    @step = 'username in use' if User.find_by_username(params[:username]).present?
   end
 end
