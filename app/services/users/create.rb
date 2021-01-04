5ff5bb5a72def3c51bcc4fa69888c98803f05399
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-04 20:03:11 -0300

    Adjust Users::Create for the transaction to work

--- app/services/users/create.rb
+++ app/services/users/create.rb
@@ -13,14 +13,11 @@ module Users
     def create_user_and_their_relationships
       ActiveRecord::Base.transaction do
         user_id = create_user&.id
-        if user_id.present?
-          create_user_interests(user_id)
-          create_user_occupations(user_id)
-          create_user_reason(user_id)
-          create_user_page_design(user_id)
-        end
+        create_user_relationships(user_id) if user_id.present?
       end
-      User.find_by_email(@params[:email]).present?
+      true
+    rescue ActiveRecord::ActiveRecordError
+      false
     end
 
     private
@@ -31,7 +28,7 @@ module Users
         @params.slice(:email, :first_name, :last_name, :location, :username, :password, :password_confirmation)
       )
       user.photo.attach(photo)
-      user.save ? user : nil
+      user.save! ? user : nil
     end
 
     def decode_photo_base64
@@ -42,26 +39,33 @@ module Users
       )
     end
 
+    def create_user_relationships(user_id)
+      create_user_interests(user_id)
+      create_user_occupations(user_id)
+      create_user_reason(user_id)
+      create_user_page_design(user_id)
+    end
+
     def create_user_interests(user_id)
       interest_ids = @params[:user_interests].split(',').map(&:to_i)
       interest_ids.each do |interest_id|
-        UserInterest.create(user_id: user_id, interest_id: interest_id)
+        UserInterest.create!(user_id: user_id, interest_id: interest_id)
       end
     end
 
     def create_user_occupations(user_id)
       occupation_ids = @params[:user_occupations].split(',').map(&:to_i)
       occupation_ids.each do |occupation_id|
-        UserOccupation.create(user_id: user_id, occupation_id: occupation_id)
+        UserOccupation.create!(user_id: user_id, occupation_id: occupation_id)
       end
     end
 
     def create_user_reason(user_id)
-      UserReason.create(user_id: user_id, reason_id: @params[:user_reason], website: @params[:website])
+      UserReason.create!(user_id: user_id, reason_id: @params[:user_reason], website: @params[:website])
     end
 
     def create_user_page_design(user_id)
-      UserPageDesign.create(user_id: user_id, design: @params[:layout].to_i, color: @params[:color].to_i)
+      UserPageDesign.create!(user_id: user_id, design: @params[:layout].to_i, color: @params[:color].to_i)
     end
   end
 end
