93263288a9d88189f4700b531a6e578428ecb7e9
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-10 15:28:09 -0300

    Generalize the occupation_text_for method to make text for interests too

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
     last_index = user.occupations.count - 1
     text = ''
     user.occupations.each_with_index do |occupation, index|
-      text += occupation_text_for(occupation, index, last_index)
+      text += text_for(occupation, index, last_index)
     end
     "#{text} in #{user.location}"
   end
@@ -23,19 +23,19 @@ module UsersHelper
   end
 
   def user_summary(user)
-    "Greetings, I'm #{user.first_name}. I'm a #{user.occupations.first.occupation} livin in #{user.location}." \
+    "Greetings, I'm #{user.first_name}. I'm a #{user.occupations.first.name} livin in #{user.location}." \
     "I am a fan of #{join_interests(user)}. You can #{get_user_reason(user)} with a click on the button above."
   end
 
   private
 
-  def occupation_text_for(occupation, occupation_index, last_index)
-    if occupation_index.zero?
-      occupation.occupation
-    elsif occupation_index < last_index
-      ", #{occupation.occupation}"
+  def text_for(object, object_index, last_index)
+    if object_index.zero?
+      object.name
+    elsif object_index < last_index
+      ", #{object.name}"
     else
-      ", and #{occupation.occupation}"
+      ", and #{object.name}"
     end
   end
 
@@ -43,18 +43,8 @@ module UsersHelper
     last_index = user.interests.count - 1
     text = ''
     user.interests.each_with_index do |interest, index|
-      text += interest_text_for(interest, index, last_index)
+      text += text_for(interest, index, last_index)
     end
     text
-  end
-
-  def interest_text_for(interest, interest_index, last_index)
-    if interest_index.zero?
-      interest.interest
-    elsif interest_index < last_index
-      ", #{interest.interest}"
-    else
-      ", and #{interest.interest}"
-    end
   end
 end
