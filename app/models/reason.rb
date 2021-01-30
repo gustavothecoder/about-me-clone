bdcc7ccbdcd88a7e68d132b052820ee7ab7d254a
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-30 19:34:54 -0300

    Improve the reason model organization

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Reason < ApplicationRecord
-  validates :reason, presence: true, uniqueness: true
-
   has_many :user_reasons
   has_many :users, through: :user_reasons
+
+  validates :reason, presence: true, uniqueness: true
 end
