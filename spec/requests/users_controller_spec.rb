4f4825d32c8ca5a28e88ab176fe6f51c9c5b9f27
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-02-13 20:09:39 -0300

    Refactor the users_controller_spec

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'base64'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe UsersController, type: :request do
   describe 'GET show' do
-    context 'When the user exists' do
-      let(:user) { create(:user, username: 'gxh') }
-      let!(:user_occupation) { create(:user_occupation, user: user, occupation_id: 1) }
-      let!(:user_reason) { create(:user_reason, user: user, reason_id: 1, website: 'example.com') }
-      let!(:user_interest) { create(:user_interest, user: user, interest_id: 1) }
-      let!(:user_page_design) { create(:user_page_design, user: user, design: 0, color: 0) }
-
-      it 'Must return HTTP status 200' do
+    context 'when the user exists' do
+      it 'should return HTTP status 200' do
+        user = create(:user, username: 'gxh')
+        create(:user_occupation, user: user, occupation_id: 1)
+        create(:user_reason, user: user, reason_id: 1, website: 'example.com')
+        create(:user_interest, user: user, interest_id: 1)
+        create(:user_page_design, user: user, design: 0, color: 0)
         get '/gxh'
         expect(response).to have_http_status(200)
       end
     end
 
-    context 'When the user does not exist' do
-      it 'Must redirect to the root' do
+    context 'when the user does not exist' do
+      it 'should redirect to the root' do
         get '/gxh'
         expect(response).to redirect_to(root_path)
       end
@@ -27,7 +25,7 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET new' do
-    it 'Must render the new template' do
+    it 'should render the new template' do
       get '/signup'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:new)
@@ -58,73 +56,55 @@ RSpec.describe 'Users', type: :request do
       }
     end
 
-    context 'When the parameters are correct' do
-      subject! { post users_path, params: params }
-
-      it 'The user and their relationships must be created' do
+    context 'when the parameters are correct' do
+      it 'the user should be created and redirected to your page' do
+        post users_path, params: params
         user = User.find_by_email(params[:email])
         expect(user).to_not be_nil
         expect(user.interests.count).to eq(3)
         expect(user.occupations.count).to eq(3)
         expect(user.user_reason).to_not be_nil
         expect(user.user_page_design).to_not be_nil
-      end
-
-      it 'The user must be redirected to your page' do
-        expect(subject).to redirect_to('http://www.example.com/gxh')
+        expect(response).to redirect_to('http://www.example.com/gxh')
       end
     end
 
-    context 'When the user parameters are not correct' do
-      subject! do
+    context 'when the user parameters are not correct' do
+      it 'the user should not be created and should be redirected to sign up page with error' do
         params[:email] = 'gxh2@email.com'
         params[:username] = 'gxh2'
         params[:last_name] = nil
         post users_path, params: params
-      end
-
-      it 'The user must not be created' do
         expect(User.find_by_email('gxh2@email.com')).to be_nil
-      end
-
-      it 'The user must be redirected to the sign up page with message: Your page cannot be created' do
-        expect(subject).to redirect_to(signup_path)
+        expect(response).to redirect_to(signup_path)
         expect(flash[:alert]).to eq('Your page cannot be created')
       end
     end
 
-    context 'When another parameter are not correct' do
-      subject! do
+    context 'when another parameter are not correct' do
+      it 'the user should not be created and should be redirected to sign up page with error' do
         params[:email] = 'gxh2@email.com'
         params[:username] = 'gxh2'
         params[:user_interests] = 'kad,akd,alkdjf'
         post users_path, params: params
-      end
-
-      it 'The user must not be created' do
         expect(User.find_by_email('gxh2@email.com')).to be_nil
-      end
-
-      it 'The user must be redirected to the sign up page with message: Your page cannot be created' do
-        expect(subject).to redirect_to(signup_path)
+        expect(response).to redirect_to(signup_path)
         expect(flash[:alert]).to eq('Your page cannot be created')
       end
     end
   end
 
   describe 'POST next_signup_step' do
-    context 'When an ajax call is made' do
-      it 'Must render the next_signup_step template' do
+    context 'when an ajax call is made' do
+      it 'should render the next_signup_step template' do
         post '/next_signup_step/2', xhr: true
         expect(response).to have_http_status(200)
         expect(response).to render_template(:next_signup_step)
       end
     end
 
-    context 'When a default request is made' do
-      it 'ActionController::UnknownFormat exception should be thrown' do
-        expect { post '/next_signup_step/2' }.to raise_error(ActionController::UnknownFormat)
-      end
+    context 'when a default request is made' do
+      it { expect { post '/next_signup_step/2' }.to raise_error(ActionController::UnknownFormat) }
     end
   end
 end
