0bad9232e7b98fad1d1b75fde2928c78444d3907
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-02-13 14:11:31 -0300

    Rename column reason to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_174641) do
+ActiveRecord::Schema.define(version: 2021_02_13_143428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_174641) do
   end
 
   create_table "reasons", force: :cascade do |t|
-    t.string "reason"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "keywords"
