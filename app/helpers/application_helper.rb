4db7f7659558e27cef8ee244332b0c6f0dd45a67
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-02-13 11:25:40 -0300

    Create the ApplicationHelper#selection_list_of

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def selection_list_of(relation)
+    content_tag(:ul, class: 'w-100 d-flex justify-content-center flex-wrap overflow-auto px-1') do
+      relation.pluck(:id, :name).each do |object|
+        concat(li_of(object))
+      end
+    end
+  end
+
+  private
+
+  def li_of(object)
+    content_tag(
+      :li,
+      object[1],
+      value: object[0],
+      class: 'select-button btn btn-outline-primary my-1 ml-1',
+      'item-to-select' => ''
+    )
+  end
 end
