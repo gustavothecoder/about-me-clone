abd3de67aa839557729f5734e36ad2d29ddb6a3f
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-02-13 20:18:23 -0300

    Organize the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,20 @@ class UsersController < ApplicationController
 
   def new; end
 
-  def create
-    if Users::Create.call(params.permit!)
-      redirect_to "/#{params[:username]}"
-    else
-      redirect_to signup_url, alert: 'Your page cannot be created'
-    end
-  end
-
   def next_signup_step
     @step = params[:step]
     @reason = Reason.find(params[:reason]) if @step == '7'
     check_that_the_data_sent_is_already_in_use
     respond_to do |format|
       format.js
+    end
+  end
+
+  def create
+    if Users::Create.call(params.permit!)
+      redirect_to "/#{params[:username]}"
+    else
+      redirect_to signup_url, alert: 'Your page cannot be created'
     end
   end
 
