29b4e26f32e93317527fd276ac6901554fae5785
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-24 20:27:00 -0300

    Create method to return the registration step

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe UsersHelper, type: :helper do
   include UsersHelper
 
-  describe 'example_users' do
-    it 'should return a UserPresenter for the luis_gama, andre_reboucas and jose_do_patrocinio' do
+  describe '#example_users' do
+    it 'returns a UserPresenter for the luis_gama, andre_reboucas and jose_do_patrocinio' do
       create(:user)
       create(:user, first_name: 'Luís', last_name: 'Gama', username: 'luis_gama')
       create(:user, first_name: 'André', last_name: 'Rebouças', username: 'andre_reboucas')
@@ -17,5 +17,20 @@ RSpec.describe UsersHelper, type: :helper do
       expect(result.second.full_name).to eq('André Rebouças')
       expect(result.third.full_name).to eq('José Do patrocínio')
     end
+  end
+
+  describe '#registration_step' do
+    it { expect(registration_step(step: 0)).to eq('zero') }
+    it { expect(registration_step(step: 1)).to eq('one') }
+    it { expect(registration_step(step: 2)).to eq('two') }
+    it { expect(registration_step(step: 3)).to eq('three') }
+    it { expect(registration_step(step: 4)).to eq('four') }
+    it { expect(registration_step(step: 5)).to eq('five') }
+    it { expect(registration_step(step: 6)).to eq('six') }
+    it { expect(registration_step(step: 7)).to eq('seven') }
+    it { expect(registration_step(step: 8)).to eq('eight') }
+    it { expect(registration_step(step: 9)).to eq('nine') }
+    it { expect(registration_step(step: 10)).to eq('ten') }
+    it { expect(registration_step(step: 11)).to eq('eleven') }
   end
 end
