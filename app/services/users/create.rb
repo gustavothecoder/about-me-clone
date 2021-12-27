4d8b96af1998c2adcdf266885ffb732b0adbc483
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-27 20:11:41 -0300

    Change how users are created

--- app/services/users/create.rb
+++ app/services/users/create.rb
@@ -18,29 +18,22 @@ module Users
 
     def create_user_and_their_relationships
       ActiveRecord::Base.transaction do
-        user_id = create_user&.id
-        create_user_relationships(user_id) if user_id.present?
+        user_id = create_user.id
+        create_user_relationships(user_id)
       end
       true
-    rescue ActiveRecord::ActiveRecordError
+    rescue StandardError
       false
     end
 
     def create_user
-      photo = decode_photo_base64
       user = User.new(
-        @params.slice(:email, :first_name, :last_name, :location, :username, :password, :password_confirmation)
+        @params.slice(
+          'email', 'first_name', 'last_name', 'location', 'username', 'password', 'password_confirmation'
+        )
       )
-      user.photo.attach(photo)
+      user.photo.attach(ActiveStorage::Blob.find_signed(@params['photo_signed_id']))
       user.save! ? user : nil
-    end
-
-    def decode_photo_base64
-      ActiveStorage::Blob.create_after_upload!(
-        io: StringIO.new(Base64.decode64(@params[:photo].split(',')[1])),
-        filename: "#{@params[:username]}.png",
-        content_type: 'image/png'
-      )
     end
 
     def create_user_relationships(user_id)
@@ -51,25 +44,25 @@ module Users
     end
 
     def create_user_interests(user_id)
-      interest_ids = @params[:user_interests].split(',').map(&:to_i)
+      interest_ids = @params['interests'].map(&:to_i)
       interest_ids.each do |interest_id|
         UserInterest.create!(user_id: user_id, interest_id: interest_id)
       end
     end
 
     def create_user_occupations(user_id)
-      occupation_ids = @params[:user_occupations].split(',').map(&:to_i)
+      occupation_ids = @params['occupations'].map(&:to_i)
       occupation_ids.each do |occupation_id|
         UserOccupation.create!(user_id: user_id, occupation_id: occupation_id)
       end
     end
 
     def create_user_reason(user_id)
-      UserReason.create!(user_id: user_id, reason_id: @params[:user_reason], website: @params[:website])
+      UserReason.create!(user_id: user_id, reason_id: @params['reason'][0], website: @params['website'])
     end
 
     def create_user_page_design(user_id)
-      UserPageDesign.create!(user_id: user_id, design: @params[:layout].to_i, color: @params[:color].to_i)
+      UserPageDesign.create!(user_id: user_id, design: @params['layout'].to_i, color: @params['color'].to_i)
     end
   end
 end
--- spec/services/users/create_spec.rb
+++ spec/services/users/create_spec.rb
@@ -3,64 +3,57 @@
 require 'rails_helper'
 
 RSpec.describe Users::Create, type: :service do
-  let(:photo_base64) do
-    photo = File.binread('public/andre_reboucas.jpg')
-    "data:image;base64,#{Base64.strict_encode64(photo)}"
-  end
   let(:params) do
     {
-      email: 'gxh@email.com',
-      first_name: 'Gustavo',
-      last_name: 'Ribeiro',
-      location: 'Brazil',
-      user_interests: '1,2,3',
-      user_occupations: '1,2,3',
-      user_reason: '1',
-      website: 'website.com',
-      photo: photo_base64,
-      layout: 1,
-      color: 1,
-      username: 'gxh',
-      password: '123456a',
-      password_confirmation: '123456a'
+      'email' => 'gxh@email.com',
+      'first_name' => 'Gustavo',
+      'last_name' => 'Ribeiro',
+      'location' => 'Brazil',
+      'interests' => %w[1 2 3],
+      'occupations' => %w[1 2 3],
+      'reason' => %w[1],
+      'website' => 'website.com',
+      'photo_signed_id' => 'blablablablablab',
+      'layout' => 1,
+      'color' => 1,
+      'username' => 'gxh',
+      'password' => '123456a',
+      'password_confirmation' => '123456a'
     }
   end
-  let!(:result) { Users::Create.call(params) }
-  let!(:created_user) { User.find_by_email(params[:email]) }
 
   context 'When all parameters are correct and are unique' do
     it 'The service must return true' do
+      photo = Rack::Test::UploadedFile.new('public/andre_reboucas.jpg')
+      allow(ActiveStorage::Blob).to receive(:find_signed).and_return(photo)
+
+      result = described_class.call(params)
+
       expect(result).to be(true)
     end
 
     it 'The user must be created with the data sent' do
-      expect(created_user).to_not be_nil
-      expect(created_user.first_name).to eq(params[:first_name])
-      expect(created_user.last_name).to eq(params[:last_name])
-      expect(created_user.location).to eq(params[:location])
-      expect(created_user.photo.attached?).to be(true)
-      expect(created_user.username).to eq(params[:username])
-      expect(created_user.password_digest).to_not be_nil
-    end
+      photo = Rack::Test::UploadedFile.new('public/andre_reboucas.jpg')
+      allow(ActiveStorage::Blob).to receive(:find_signed).and_return(photo)
+      described_class.call(params)
 
-    it 'User interests must be Photography, Technology and Design' do
+      created_user = User.find_by_email('gxh@email.com')
+
+      expect(created_user).to_not be_nil
+      expect(created_user.first_name).to eq(params['first_name'])
+      expect(created_user.last_name).to eq(params['last_name'])
+      expect(created_user.location).to eq(params['location'])
+      expect(created_user.photo.attached?).to be(true)
+      expect(created_user.username).to eq(params['username'])
+      expect(created_user.password_digest).to_not be_nil
       expect(created_user.interests.first.name).to eq('Photography')
       expect(created_user.interests.second.name).to eq('Technology')
       expect(created_user.interests.third.name).to eq('Design')
-    end
-
-    it 'User occupations must be Student, Web Developer and Software Engineer' do
       expect(created_user.occupations.first.name).to eq('Student')
       expect(created_user.occupations.second.name).to eq('Web Developer')
       expect(created_user.occupations.third.name).to eq('Software Engineer')
-    end
-
-    it 'User reason must be Attend my event and the website must be website.com' do
       expect(created_user.user_reason.reason.name).to eq('Attend my event')
       expect(created_user.user_reason.website).to eq('website.com')
-    end
-
-    it 'The layout must be medium and the color must be pink' do
       expect(created_user.user_page_design.design).to eq('medium')
       expect(created_user.user_page_design.color).to eq('pink')
     end
@@ -68,21 +61,25 @@ RSpec.describe Users::Create, type: :service do
 
   context 'When there is a problem with the user parameters' do
     it 'The user must not be created' do
-      params[:email] = 'gxh2@email.com'
-      params[:username] = 'gxh2'
-      params[:first_name] = nil
-      result = Users::Create.call(params)
+      params['first_name'] = nil
+      photo = Rack::Test::UploadedFile.new('public/andre_reboucas.jpg')
+      allow(ActiveStorage::Blob).to receive(:find_signed).and_return(photo)
+
+      result = described_class.call(params)
+
       expect(result).to be(false)
-      expect(User.find_by_email(params[:email])).to be_nil
+      expect(User.find_by_email(params['email'])).to be_nil
     end
   end
 
   context 'When there is a problem with the another parameter' do
     it 'The user must not be created' do
-      params[:email] = 'gxh2@email.com'
-      params[:username] = 'gxh2'
-      params[:user_interests] = '3249,392,8934'
-      result = Users::Create.call(params)
+      params['interests'] = %w[3249 392 8934]
+      photo = Rack::Test::UploadedFile.new('public/andre_reboucas.jpg')
+      allow(ActiveStorage::Blob).to receive(:find_signed).and_return(photo)
+
+      result = described_class.call(params)
+
       expect(result).to be(false)
       expect(User.find_by_email(params[:email])).to be_nil
     end
