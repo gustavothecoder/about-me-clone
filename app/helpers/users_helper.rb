1c183d7112d04499d817ec3599fbbdfa930cd329
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-30 15:48:03 -0300

    Move the user examples search to the Users::Examples#index action

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,12 +5,6 @@ module UsersHelper
     zero one two three four five six seven eight nine ten eleven
   ].freeze
 
-  def example_users
-    User.where(
-      "username IN ('luis_gama', 'andre_reboucas', 'jose_do_patrocinio', 'angela_davis')"
-    ).map { |user| UserPresenter.new(user) }
-  end
-
   def registration_step(step:)
     REGISTRATION_STEPS[step]
   end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -5,20 +5,6 @@ require 'rails_helper'
 RSpec.describe UsersHelper, type: :helper do
   include UsersHelper
 
-  describe '#example_users' do
-    it 'returns a UserPresenter for the luis_gama, andre_reboucas and jose_do_patrocinio' do
-      create(:user)
-      create(:user, first_name: 'Luís', last_name: 'Gama', username: 'luis_gama')
-      create(:user, first_name: 'André', last_name: 'Rebouças', username: 'andre_reboucas')
-      create(:user, first_name: 'José', last_name: 'do Patrocínio', username: 'jose_do_patrocinio')
-      result = example_users
-      expect(result.count).to eq(3)
-      expect(result.first.full_name).to eq('Luís Gama')
-      expect(result.second.full_name).to eq('André Rebouças')
-      expect(result.third.full_name).to eq('José Do patrocínio')
-    end
-  end
-
   describe '#registration_step' do
     it { expect(registration_step(step: 0)).to eq('zero') }
     it { expect(registration_step(step: 1)).to eq('one') }
