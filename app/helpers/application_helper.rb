16b2429af1ef9168167ed7796324430f7a24bc44
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-30 15:52:42 -0300

    Move the #registration_step to the ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  REGISTRATION_STEPS = %w[
+    zero one two three four five six seven eight nine ten eleven
+  ].freeze
+
   def selection_list_of(relation)
     content_tag(:ul, class: 'w-100 d-flex justify-content-center flex-wrap overflow-auto px-1') do
       relation.pluck(:id, :name).each do |object|
         concat(li_of(object))
       end
     end
+  end
+
+  def registration_step(step:)
+    REGISTRATION_STEPS[step]
   end
 
   private
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -51,4 +51,19 @@ RSpec.describe ApplicationHelper, type: :helper do
       it { is_expected.to include(Reason.third.name) }
     end
   end
+
+  describe '#registration_step' do
+    it { expect(registration_step(step: 0)).to eq('zero') }
+    it { expect(registration_step(step: 1)).to eq('one') }
+    it { expect(registration_step(step: 2)).to eq('two') }
+    it { expect(registration_step(step: 3)).to eq('three') }
+    it { expect(registration_step(step: 4)).to eq('four') }
+    it { expect(registration_step(step: 5)).to eq('five') }
+    it { expect(registration_step(step: 6)).to eq('six') }
+    it { expect(registration_step(step: 7)).to eq('seven') }
+    it { expect(registration_step(step: 8)).to eq('eight') }
+    it { expect(registration_step(step: 9)).to eq('nine') }
+    it { expect(registration_step(step: 10)).to eq('ten') }
+    it { expect(registration_step(step: 11)).to eq('eleven') }
+  end
 end
